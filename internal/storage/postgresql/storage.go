e1678e96c357aa902c6468755a1d5082f743422e
vpg1 <malihtorovich@mail.ru>
2024-10-19 23:51:32 +0300

    Add storage implementation

---
+++ internal/storage/postgresql/storage.go
@@ -0,0 +1,33 @@
+package postgresql
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"url-shortener/internal/config"
+)
+
+type Logger interface {
+	Info(string, ...any)
+	Debug(string, ...any)
+	Warn(string, ...any)
+	Error(string, ...any)
+}
+
+type Storage struct {
+	db     *sqlx.DB
+	logger *Logger
+}
+
+func NewStorage(pgConfig config.PostgresServer, logger *Logger) *Storage {
+	connString := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable",
+		pgConfig.User, pgConfig.Password, pgConfig.Address, pgConfig.Port, pgConfig.DbName)
+
+	conn, err := sqlx.Connect("postgres", connString)
+	if err != nil {
+		(*logger).Error("Error connecting to database")
+	}
+
+	(*logger).Debug("Connected to database")
+
+	return &Storage{conn, logger}
+}
